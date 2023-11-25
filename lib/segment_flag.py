c7873d83cf64956690a3db93eebd8258eb0515a8
Genvictus <johannstarkmk42@gmail.com>
2023-11-25 20:12:17 +0700

    rfc: changed properties & add setflag method

--- lib/segment_flag.py
+++ lib/segment_flag.py
@@ -1,26 +1,29 @@
 from dataclasses import dataclass
 import struct
 
-from .constants import ENDIAN_SPECIFIER
-
-# Constants
-ACK_FLAG = 0b00010000
-SYN_FLAG = 0b00000010
-FIN_FLAG = 0b00000001
-
+from .constants import *
 
 @dataclass
 class SegmentFlag:
-    __ack: bool
-    __syn: bool
-    __fin: bool
+    ack: bool
+    syn: bool
+    fin: bool
 
-    def __init__(self, flag: bytes):
+    def __init__(self, flag: bytes = b'\x00'):
         # Init flag variable from flag byte
         int_flag: int = struct.unpack(f"{ENDIAN_SPECIFIER}b", flag)[0]
-        self.__ack = bool(int_flag & ACK_FLAG)
-        self.__syn = bool(int_flag & SYN_FLAG)
-        self.__fin = bool(int_flag & FIN_FLAG)
+        self.ack = bool(int_flag & ACK_FLAG)
+        self.syn = bool(int_flag & SYN_FLAG)
+        self.fin = bool(int_flag & FIN_FLAG)
+
+    def set_flags(self, flags: list[int]):
+        int_flag: int = 0
+        for flag in flags:
+            int_flag |= flag
+
+        self.ack = bool(int_flag & ACK_FLAG)
+        self.syn = bool(int_flag & SYN_FLAG)
+        self.fin = bool(int_flag & FIN_FLAG)
 
     def get_flag_bytes(self) -> bytes:
         # Convert this object to flag in byte form
