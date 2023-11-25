a911a39f4d157672174777d7689e93a8500c9b9d
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-11-26 00:06:05 +0700

    feat : segment conversion

--- lib/segment.py
+++ lib/segment.py
@@ -7,24 +7,30 @@ CRC_POL = 0b11000000000000101
 
 @dataclass
 class Segment:
-    flags: SegmentFlag
     seq_num: int
     ack_num: int
-    checksum: bytes
+    flags: SegmentFlag
+    checksum: int
     payload: bytes
 
     # -- Internal Function --
-    def __init__(self, seq_num: int = 0, ack_num: int = 0,payload: bytes = b""):
+    def __init__(self, seq_num: int = 0, ack_num: int = 0, flags: bytes = b"\x00"):
         # Initalize segment
-        self.flags = SegmentFlag()
         self.seq_num = seq_num
         self.ack_num = ack_num
-        self.payload = payload
+        self.flags = SegmentFlag(flags)
+        self.checksum = 0
+        self.payload = b""
 
     def __str__(self):
-        # Optional, override this method for easier print(segmentA)
         output = ""
-        output += f"{'Sequence number':24} | {self.sequence}\n"
+        output += f"{'Seq Num':12}\t\t| {self.seq_num}\n"
+        output += f"{'Ack Num':12}\t\t| {self.ack_num}\n"
+        output += f"{'Flag SYN':12}\t\t| {self.flags.syn}\n"
+        output += f"{'Flag ACK':12}\t\t| {self.flags.ack}\n"
+        output += f"{'Flag FIN':12}\t\t| {self.flags.fin}\n"
+        output += f"{'Checksum':24}| {self.checksum}\n"
+        output += f"{'MsgSize':24}| {len(self.payload)}\n"
         return output
 
     @staticmethod
@@ -35,17 +41,16 @@ class Segment:
         return segment
 
     @staticmethod
-    def ack(seq_num: int, ack_num: int) -> 'Segment':
+    def ack(seq_num: int, ack_num: int, payload: bytes = b"") -> 'Segment':
         segment = Segment(seq_num= seq_num, ack_num= ack_num)
-
         segment.flags.set_flags([ACK_FLAG])
+        segment.payload = payload
 
         return segment
     
     @staticmethod
     def syn_ack(seq_num: int, ack_num: int) -> 'Segment':
         segment = Segment(seq_num= seq_num, ack_num= ack_num)
-
         segment.flags.set_flags([SYN_FLAG, ACK_FLAG])
 
         return segment
@@ -60,12 +65,11 @@ class Segment:
     @staticmethod
     def fin_ack(seq_num: int, ack_num: int) -> 'Segment':
         segment = Segment(seq_num= seq_num, ack_num= ack_num)
-
         segment.flags.set_flags([ACK_FLAG, FIN_FLAG])
 
         return segment
 
-    def __calculate_checksum(self) -> bytes:
+    def __calculate_checksum(self) -> int:
         #CRC-16/CCITT-FALSE
         CRC_XOR_INIT = 0xFFFF  
         CRC_XOR_OUT = 0x0000 
@@ -91,7 +95,67 @@ class Segment:
 
     def is_valid_checksum(self) ->bool:
         return (self.checksum == self.__calculate_checksum())
+    
+    
+    @staticmethod
+    def parse_from_bytes(data: bytes) -> 'Segment':
+        seq_num = struct.unpack('I', data[0:4])[0]
+        ack_num = struct.unpack('I', data[4:8])[0]
+        flags = data[8:9]
+        checksum = struct.unpack('H', data[10:12])[0]
 
-segment = Segment(payload=b"hello world")
-segment.update_checksum()
-print(segment.checksum)
+        payload = data[12:]
+
+
+        segment = Segment(seq_num, ack_num, flags)
+        segment.checksum = checksum
+        segment.payload = payload
+
+        return segment
+    
+    @staticmethod
+    def convert_to_byte(segment: 'Segment') -> bytes:
+        data = b""
+        data += struct.pack('II', segment.seq_num, segment.ack_num)
+        data += segment.flags.get_flag_bytes()
+        data += struct.pack('x')
+        data += struct.pack('H', segment.checksum)
+        data += segment.payload
+
+        return data
+    
+
+# segment = Segment(seq_num=0, ack_num=1)
+# segment.payload = b"bsjdasdjahd testtt"
+# print(segment)
+# print(segment.payload)
+# segment_byte = Segment.convert_to_byte(segment)
+# print("")
+# print(segment_byte)
+
+# segment2 = Segment.parse_from_bytes(segment_byte)
+# print(segment2)
+# print(segment2.payload)
+
+# print("segment")
+# print(segment)
+
+# syn = segment.syn(segment.seq_num)
+# print("syn")
+# print(syn)
+
+# ack = segment.ack(segment.seq_num, segment.ack_num)
+# print("ack")
+# print(ack)
+
+# syn_ack = segment.syn_ack(segment.seq_num, segment.ack_num)
+# print("syn_ack")
+# print(syn_ack)
+
+# fin = segment.fin(segment.seq_num)
+# print("syn")
+# print(fin)
+
+# fin_ack = segment.fin_ack(segment.seq_num, segment.ack_num)
+# print("fin_ack")
+# print(fin_ack)
--- lib/segment_flag.py
+++ lib/segment_flag.py
@@ -11,7 +11,7 @@ class SegmentFlag:
 
     def __init__(self, flag: bytes = b'\x00'):
         # Init flag variable from flag byte
-        int_flag: int = struct.unpack(f"{ENDIAN_SPECIFIER}b", flag)[0]
+        int_flag: int = struct.unpack(f"{ENDIAN_SPECIFIER}B", flag)[0]
         self.ack = bool(int_flag & ACK_FLAG)
         self.syn = bool(int_flag & SYN_FLAG)
         self.fin = bool(int_flag & FIN_FLAG)
@@ -28,13 +28,13 @@ class SegmentFlag:
     def get_flag_bytes(self) -> bytes:
         # Convert this object to flag in byte form
         int_flag: int = 0
-        if self.__ack:
+        if self.ack:
             int_flag |= ACK_FLAG
 
-        if self.__syn:
+        if self.syn:
             int_flag |= SYN_FLAG
 
-        if self.__fin:
+        if self.fin:
             int_flag |= FIN_FLAG
 
-        return struct.pack(f"{ENDIAN_SPECIFIER}b", int_flag)
+        return struct.pack(f"{ENDIAN_SPECIFIER}B", int_flag)
