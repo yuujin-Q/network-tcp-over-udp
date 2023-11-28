7f4c798a1713f9a25a79744acdfd4db47aa176eb
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-11-28 20:31:49 +0700

    feat: clientinstance.py

--- clientinstance.py
+++ clientinstance.py
@@ -1,6 +1,48 @@
 import argparse
+import struct
 
-from lib.constants import LOOPBACK_ADDR
+from lib.address import Address
+from lib.constants import *
+from node.client import Client
+
+
+class ClientInstance:
+    def __init__(self, dest_ip: str, dest_port: int, self_ip: str = LOOPBACK_ADDR, self_port: int = DEFAULT_PORT):
+        self._dest_ip = dest_ip
+        self._addr = Address(self_ip, self_port)
+        self._connection: Client = Client(dest_ip, dest_port, self_ip, self_port)
+        self._server_handler: Address | None = None
+
+    def request_connection(self):
+        # handshake to broadcast port
+        self._connection.three_way_handshake()
+
+        # redirect response
+        response = self._connection.await_segment()
+        # TODO fin for close connection
+        self._connection.close_connection()
+
+        # get new port
+        new_port = struct.unpack(f'{ENDIAN_SPECIFIER}I', response.segment.payload)[0]
+
+        # create new connection
+        self._connection = Client(self._dest_ip, new_port, self._addr.get_ip(), self._addr.get_port())
+
+    def run_receiver(self):
+        # handshake to handler
+        dest = self._connection.three_way_handshake()
+
+        # receive file
+        data = self._connection.start_receiver_transfer(dest)
+
+        # save file
+        filename = input('[?] Input filename: ')
+        with open(filename, "wb") as file:
+            file.write(data)
+
+    def close_connection(self):
+        self._connection.close_connection()
+
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description='client host for file transfer')
@@ -19,3 +61,7 @@ if __name__ == "__main__":
     input('[?] Press ENTER to start client')
     print(f'[!] Starting client on {src_ip}:{src_port}')
     print(f'[!] Connecting to server on {dest_ip}:{dest_port}')
+    client = ClientInstance(dest_ip, dest_port, src_ip, src_port)
+    client.request_connection()
+    client.run_receiver()
+    client.close_connection()
