070a1760b3885944cd5edf8965c4e5f493ab55f5
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-11-28 22:08:58 +0700

    feat: connect tictactoe

--- game/tictactoe.py
+++ game/tictactoe.py
@@ -1,3 +1,7 @@
+import struct
+from node.serverhandler import *
+from node.client import *
+
 def show_board(matrix):
     for i in range(3):
         for j in range(3):
@@ -47,7 +51,7 @@ def is_valid_input(i,j):
     return (i>=0 and i<3) and (j>=0 and j<3) 
 
 
-def input_board(board):
+def input_board(board, turn):
     i = int(input('row      : '))
     j = int(input('column   : '))
     print('')
@@ -59,7 +63,7 @@ def input_board(board):
     
     return i,j
 
-def tictactoe():
+def tictactoe(Marker: chr, handler: Host, opp_addr: Address):
     board = initiate_board()
 
     win = ' '
@@ -69,7 +73,17 @@ def tictactoe():
     while win == ' ' and not(is_board_full(board)):
         show_board(board)
         print('its ' + turn + ' turn')
-        i, j = input_board(board)
+
+        if(turn == Marker):
+            i, j = input_board(board)
+            input_loc = game_input(i,j)
+            handler.send_payload(input_loc.convert_to_bytes(), opp_addr)
+        else:
+            message = handler.await_segment()
+            bytes = message.segment.payload
+
+            i, j = struct.unpack('II', bytes)
+
         board[i][j] = turn
 
         win = winner(board)
@@ -86,6 +100,66 @@ def tictactoe():
     else:
         print('tie')
 
+class game_input:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+    
+    def convert_to_bytes(self)->bytes:
+        # segment = Segment() #TODO : seq_num ack_num
+        # data = b""
+        # data += struct.pack('II', self.i, self.j)
+        # segment.set_payload(data)
+
+        return struct.pack('II', self.i, self.j)
+
+        
 
 
-tictactoe()
+
+if __name__ == "__main__":
+    print("Selamat datang di tictactoe!")
+    print("Ketik 1 jika kamu ingin memulai permainan baru!")
+    print("Ketik 2 jika kamu ingin join ke permain lain!")
+
+    jawaban = int(input("Jawaban kamu: "))
+
+    while(jawaban > 2 and jawaban < 1 ):
+        print('invalid input! ulangi!')
+        jawaban = int(input("Jawaban kamu: "))
+
+    if(jawaban == 1):
+        port = int(input("Pilih port hosting permainan(default): "))
+        handler = ServerHandler(self_port= port)
+        
+        print("waiting for opponent")
+        dest = handler.three_way_handshake()
+
+        print('opponent found')
+        Marker = input('Masukkan marker yang kamu mau: [X/O]')
+        byte_marker = struct.pack('c', Marker)
+        handler.send_payload(byte_marker)
+
+        tictactoe(Marker, handler, dest)
+
+
+    elif(jawaban == 2):
+        port = int(input("Pilih port untuk koneksi: "))
+        dest_ip = input("masukkan ip host: ") 
+        dest_port = int(input("masukkan port host: "))
+        handler = Client(dest_ip, dest_port, self_port= port)
+        
+        print("waiting for opponent")
+        handler = ServerHandler(self_port=SERVER_BROADCAST_PORT)
+        dest = handler.three_way_handshake()
+
+        message = handler.await_segment()
+
+        Marker = struct.unpack('c', message.segment.payload)
+        print('Selamat bermain')
+
+        tictactoe(Marker, handler, dest)
+    
+    handler.send_fin_segment(dest)
+    handler.close_connection()
+
